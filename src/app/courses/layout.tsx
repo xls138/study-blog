353b3a96e3a187dab2936c9245b41212264d3c83
xiaolisheng <1522546196@qq.com>
2025-09-03 19:00:56 +0800

    feat: add JavaScript First Steps course and update course layout

--- src/app/courses/layout.tsx
+++ src/app/courses/layout.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/Header";
+
 export default function CoursesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 }
